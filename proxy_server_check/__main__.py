582a64c645d3b041e006345d31ace4c219746fe8
wangli2000_cn <wangli2000_cn@126.com>
2019-08-19 10:16:49 +0800

    201908190000

--- proxy_server_check/__main__.py
+++ proxy_server_check/__main__.py
@@ -40,20 +40,21 @@ if __name__ == "__main__":
 
                 print('当前测试代理服务器'+str(flag)+':'+server.p_ip)
                 sel=Sel('Chrome',db_check_session,SystemPar,server.p_ip+':'+str(server.p_port))
-                net_test_dist=SystemPar.get_value(db_session,'net_test_dist')
-                net_test_dist_exists_text=SystemPar.get_value(db_session,'net_test_dist_exists_text')
-                server.p_lastcheck_time=datetime.datetime.now()
+                net_test_dist=SystemPar.get_value(db_check_session,'net_test_dist')
+                net_test_dist_exists_text=SystemPar.get_value(db_check_session,'net_test_dist_exists_text')
+                server_check=db_check_session.query(ProxyServer).filter(ProxyServer.p_port==server.p_port,ProxyServer.p_ip==server.p_ip,ProxyServer.p_inuse==True,ProxyServer.p_type=="HTTP").all()
+                server_check.p_lastcheck_time=datetime.datetime.now()
                 if (net_test_dist_exists_text in sel.getHtmlSource(net_test_dist)):
-                    server.p_lastcheck_status="可用"
+                    server_check.p_lastcheck_status="可用"
                 else:
-                    server.p_lastcheck_status="不可用"
+                    server_check.p_lastcheck_status="不可用"
                 sel.closeWindow()
                 db_check_session.commit()
             except:
                 db_session.rollback()
                 raise
             finally:
-                  db_check_session.close()
+                db_check_session.close()
 
         db_session.commit()
     except:
